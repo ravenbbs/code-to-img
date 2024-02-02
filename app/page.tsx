bdf20a0c50974330b4fd60cbd1d306e42b99b3c6
ravenbbs <julioc3bgud@gmail.com>
2024-02-01 23:21:48 -0500

    Import ace themes - ace languages

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import CodeEditor from "@/components/CodeEditor";
 export default function Home() {
   return (
     <main>
-      <div className="code-editor-ref  border-2 bg-slate-500">
+      <div className="code-editor-ref">
         <CodeEditor />
       </div>
     </main>
--- components/CodeEditor.tsx
+++ components/CodeEditor.tsx
@@ -3,6 +3,21 @@ import React from "react";
 import { Resizable } from "re-resizable";
 import AceEditor from "react-ace";
 
+//languages}
+import "ace-builds/src-noconflict/mode-html"
+import "ace-builds/src-noconflict/mode-javascript"
+import "ace-builds/src-noconflict/mode-css"
+import "ace-builds/src-noconflict/mode-python"
+import "ace-builds/src-noconflict/mode-java"
+import "ace-builds/src-noconflict/mode-typescript"
+
+//themes
+import "ace-builds/src-noconflict/theme-monokai"
+import "ace-builds/src-noconflict/theme-solarized_dark"
+import "ace-builds/src-noconflict/theme-twilight"
+import "ace-builds/src-noconflict/theme-terminal"
+import "ace-builds/src-noconflict/theme-kuroir"
+
 function CodeEditor() {
   return (
     <Resizable
@@ -13,12 +28,14 @@ function CodeEditor() {
     >
       <div>
         <AceEditor
+          theme="monokai"
+          mode={"javascript"}
           wrapEnabled={true}
           showPrintMargin={false}
-          highlightActiveLine={false}
+          highlightActiveLine={true}
           fontSize={16}
           className="ace-editor-container"
-          showGutter={false}
+          showGutter={true}
           name="UNIQUE_ID_OF_DIV"
           editorProps={{ $blockScrolling: true }}
           value="function CodeEditor() {hello world}"
