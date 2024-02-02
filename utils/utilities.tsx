896d4f1dff935057c3e3d6ef8f405bfd3d1680d3
ravenbbs <julioc3bgud@gmail.com>
2024-02-01 23:21:14 -0500

    Utilities - Languages

---
+++ utils/utilities.tsx
@@ -0,0 +1,30 @@
+export const languages = [
+  {
+    name: "JavaScript",
+    icon: "icons/javascript.svg"
+  },
+  {
+    name: "Html",
+    icon: "icons/html.svg"
+  },
+  {
+    name: "Css",
+    icon: "icons/css.svg"
+  },
+  {
+    name: "Java",
+    icon: "icons/java.svg"
+  },
+  {
+    name: "Json",
+    icon: "icons/json.svg"
+  },
+  {
+    name: "Python",
+    icon: "icons/python.svg"
+  },
+  {
+    name: "TypeScript",
+    icon: "icons/typescript.svg"
+  },
+]
