233852fdea8db300a013d12ed04dce1d0c801c25
ravenbbs <julioc3bgud@gmail.com>
2024-02-01 22:41:31 -0500

    Add React resizable - AceEditor

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import CodeEditor from "@/components/CodeEditor";
-import Image from "next/image";
 
 export default function Home() {
   return (
-  <main> 
-    <div className="code-editor-ref">
-    
-    <CodeEditor/>
-    </div>
-
-  </main>);
+    <main>
+      <div className="code-editor-ref  border-2 bg-slate-500">
+        <CodeEditor />
+      </div>
+    </main>
+  );
 }
--- components/CodeEditor.tsx
+++ components/CodeEditor.tsx
@@ -1,7 +1,31 @@
-"use client"
+"use client";
+import React from "react";
+import { Resizable } from "re-resizable";
+import AceEditor from "react-ace";
 
 function CodeEditor() {
-  return <div>Edit</div>;
+  return (
+    <Resizable
+      className="bg-white m-2"
+      minHeight={466}
+      minWidth={510}
+      maxWidth={1000}
+    >
+      <div>
+        <AceEditor
+          wrapEnabled={true}
+          showPrintMargin={false}
+          highlightActiveLine={false}
+          fontSize={16}
+          className="ace-editor-container"
+          showGutter={false}
+          name="UNIQUE_ID_OF_DIV"
+          editorProps={{ $blockScrolling: true }}
+          value="function CodeEditor() {hello world}"
+        />
+      </div>
+    </Resizable>
+  );
 }
 
-export default CodeEditor
+export default CodeEditor;
