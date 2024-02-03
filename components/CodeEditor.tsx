8d6cbff69d5b31c106923d14f2f1e22f6bc68551
ravenbbs <julioc3bgud@gmail.com>
2024-02-02 22:26:01 -0500

    resize content

--- components/CodeEditor.tsx
+++ components/CodeEditor.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Resizable } from "re-resizable";
 import AceEditor from "react-ace";
 
@@ -34,26 +34,37 @@ function CodeEditor({
   icon,
   background,
   currentPadding}:CodeEditorProps) {
+
+    const [width, setWidth] = React.useState(1000)
+    const [height, setHeight] = React.useState<number | null>(500)
+    //@ts-ignore
+    const handleResize = (evt, direction, ref, pos ) => {}
+
   return (
     <Resizable
       minHeight={466}
       minWidth={510}
       maxWidth={1000}
+      defaultSize={{
+        width: width,
+        height: height || 500
+      }}
+      onResize={handleResize}
     >
       <div>
         <AceEditor
+          value="function CodeEditor() {hello world}"
+          name="UNIQUE_ID_OF_DIV"
+          fontSize={18}
           theme="monokai"
           mode={language.toLocaleLowerCase()}
+          showGutter={false}
           wrapEnabled={true}
           showPrintMargin={false}
-          highlightActiveLine={true}
-          fontSize={16}
-          className="ace-editor-container"
-          showGutter={true}
-          name="UNIQUE_ID_OF_DIV"
+          highlightActiveLine={false}
           editorProps={{ $blockScrolling: true }}
-          value="function CodeEditor() {hello world}"
-        />
+          className="ace-editor-container"
+          />
       </div>
     </Resizable>
   );
