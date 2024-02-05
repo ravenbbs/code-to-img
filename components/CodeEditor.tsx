872fd2c4fa9834c87ef87b1c16c4e0358f7d256a
Julio Condor <julioc3bgud@gmail.com>
2024-02-04 21:07:33 -0500

    Resize - width & height

--- components/CodeEditor.tsx
+++ components/CodeEditor.tsx
@@ -1,30 +1,30 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Resizable } from "re-resizable";
 import AceEditor from "react-ace";
 
 //languages}
-import "ace-builds/src-noconflict/mode-html"
-import "ace-builds/src-noconflict/mode-javascript"
-import "ace-builds/src-noconflict/mode-css"
-import "ace-builds/src-noconflict/mode-python"
-import "ace-builds/src-noconflict/mode-java"
-import "ace-builds/src-noconflict/mode-typescript"
+import "ace-builds/src-noconflict/mode-html";
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/mode-css";
+import "ace-builds/src-noconflict/mode-python";
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/mode-typescript";
 
 //themes
-import "ace-builds/src-noconflict/theme-monokai"
-import "ace-builds/src-noconflict/theme-solarized_dark"
-import "ace-builds/src-noconflict/theme-twilight"
-import "ace-builds/src-noconflict/theme-terminal"
-import "ace-builds/src-noconflict/theme-kuroir"
+import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/theme-solarized_dark";
+import "ace-builds/src-noconflict/theme-twilight";
+import "ace-builds/src-noconflict/theme-terminal";
+import "ace-builds/src-noconflict/theme-kuroir";
 
 interface CodeEditorProps {
   onCodeChange: (code: string) => void;
   language: string;
   theme: string;
-  icon: string
-  background?: string
-  currentPadding?: string
+  icon: string;
+  background?: string;
+  currentPadding?: string;
 }
 
 function CodeEditor({
@@ -33,12 +33,25 @@ function CodeEditor({
   theme,
   icon,
   background,
-  currentPadding}:CodeEditorProps) {
+  currentPadding,
+}: CodeEditorProps) {
+  const [width, setWidth] = React.useState(1000);
+  const [height, setHeight] = React.useState<number | null>(500);
+  //@ts-ignore
+  const handleResize = (evt, direction, ref, pos) => {
+    const newHeight = ref.style.height;
+    setHeight(parseInt(newHeight));
+  };
 
-    const [width, setWidth] = React.useState(1000)
-    const [height, setHeight] = React.useState<number | null>(500)
-    //@ts-ignore
-    const handleResize = (evt, direction, ref, pos ) => {}
+  const updateSize = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateSize);
+    updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
 
   return (
     <Resizable
@@ -47,11 +60,25 @@ function CodeEditor({
       maxWidth={1000}
       defaultSize={{
         width: width,
-        height: height || 500
+        height: height || 500,
       }}
       onResize={handleResize}
+      className="resize-container relative"
     >
-      <div>
+      <div className="code-block">
+        <div className="code-title h-[52px] px-4 flex items-center justify-between bg-black bg-opacity-80">
+          <div className="dots flex items-center gap-1">
+            <div className="w-3 h-3 rounded-full bg-[#FF5D5B] border border-[#CF544D]"></div>
+            <div className="w-3 h-3 rounded-full bg-[#FFBB39] border border-[#CFA64E]"></div>
+            <div className="w-3 h-3 rounded-full bg-[#00CD4E] border border-[#0EA642]"></div>
+          </div>
+          <div className="input-control w-full">
+            <input type="text" className="w-full text-[hsla(0,0%,100%,.6)] outline-none font-medium text-center bg-transparent" />
+          </div>
+          <div className="icon flex justify-center items-center p-1 bg-black bg-opacity-30 rounded-sm">
+            <img src={icon} alt='' />
+          </div>
+        </div>
         <AceEditor
           value="function CodeEditor() {hello world}"
           name="UNIQUE_ID_OF_DIV"
@@ -64,7 +91,7 @@ function CodeEditor({
           highlightActiveLine={false}
           editorProps={{ $blockScrolling: true }}
           className="ace-editor-container"
-          />
+        />
       </div>
     </Resizable>
   );
