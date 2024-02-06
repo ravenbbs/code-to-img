c1c21ddc40b522da513c04271dac205a38237489
Julio Condor <julioc3bgud@gmail.com>
2024-02-05 23:39:08 -0500

    dropdown style

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,12 @@ export default function Home() {
   const [language, setLanguage] = useState(languages[0].name)
   const [activeIcon, setActiveIcon] = useState(languages[0].icon)
   return (
-    <main className="h-[100vh] flex flex-col items-center justify-between">
+    <main className="h-[100vh] flex flex-col items-center justify-between ">
     <header className=" mt-6 w-[940px] p-5 fixed top-0 left-1/2 translate-x-[-50%]
       x-10 bg-[#191919] rounded border border-[#3C3C3C] shadow-md">
       <LanguageSelector language={language} setLanguage={setLanguage} setActiveIcon={setActiveIcon} />
     </header>
-      <div className="code-editor-ref mt-[14rem]">
+      <div className="code-editor-ref mt-[14rem] -z-10">
         <CodeEditor language={language} icon={activeIcon} />
       </div>
     </main>
