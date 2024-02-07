a79e9fbf9fc0ba832bb817c2bd05601583b86cc6
Julio Condor <julioc3bgud@gmail.com>
2024-02-07 18:29:58 -0500

    Padding selector functionality

--- components/PaddingSelector.tsx
+++ components/PaddingSelector.tsx
@@ -13,7 +13,31 @@ function PaddingSelector({
   currentPadding,
   setCurrentPadding
 }: PaddingSelectorProps) {
-  return <div>Padding selctor</div>
+
+  const changePadding = (newPadding: string) => {
+    setCurrentPadding(newPadding);
+
+  };
+
+  return (    
+  <div>
+    <p className="py-[5px] text-sm font-medium">Padding</p>
+    <div className="flex gap-2">
+       {paddings.map((padding, i) => {
+      return (
+      <button 
+      className={`hover-item rounded-md ${currentPadding == padding ? 'bg-[#3f3f3f] text-[#cccccc]' : '' } `}
+      onClick={() => changePadding(padding)} 
+      key={i}>
+        {padding}
+      </button>
+      )
+    })}
+    </div>
+   
+
+  </div>
+)
 }
 
 export default PaddingSelector
