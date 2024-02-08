329a18c76cff835eef8a62d9b5f02388b0d1b7b0
Julio Condor <julioc3bgud@gmail.com>
2024-02-07 22:31:56 -0500

    Add footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React from "react"
+
+function Footer() {
+  return (
+    <footer>
+      <p className="py-8 font-semibold cursor-default">
+        Made by <a href="https://github.com/ravenbbs" target="_blank" className="cursor-pointer hover:text-indigo-500 transition-all duration-300">Julio Condor</a>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
