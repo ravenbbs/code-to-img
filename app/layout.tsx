1ddc46e43349e3bef7d85b86ca51183115213b62
Julio Condor <julioc3bgud@gmail.com>
2024-02-09 20:19:36 -0500

    Try fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -10,15 +9,15 @@ export const metadata: Metadata = {
   title: 'Code To Image App',
   description: 'Convierte tu código en imágenes de forma rápida y sencilla con Code To Image App. Ideal para compartir tus proyectos en redes sociales y presentaciones.',
   // Favicons - Icons
-  icons: {
-    icon: '/icon.png',
-    shortcut: '/shortcut-icon.png',
-    apple: '/apple-icon.png',
-    other: {
-      rel: 'apple-touch-icon-precomposed',
-      url: '/apple-touch-icon-precomposed.png',
-    },
-  },
+  // icons: {
+  //   icon: '/icon.png',
+  //   shortcut: '/shortcut-icon.png',
+  //   apple: '/apple-icon.png',
+  //   other: {
+  //     rel: 'apple-touch-icon-precomposed',
+  //     url: '/apple-touch-icon-precomposed.png',
+  //   },
+  // },
 
   referrer: 'origin-when-cross-origin',
   keywords: ['code', 'código', 'image', 'imagen', 'conversor', 'converter', 'desarrollo', 'programación', 'compartir', 'redes sociales', 'presentaciones'],
@@ -40,23 +39,20 @@ export const metadata: Metadata = {
         alt: 'Image of code',
       },
     ],
-    locale: 'es_ES',
     type: 'website',
   },
 
 // twitter Tags
-  twitter: {
-    card: 'app',
-    title: 'Code To Image App',
-    description: 'Convierte tu código en imágenes de forma rápida y sencilla con Code To Image App. Ideal para compartir tus proyectos en redes sociales y presentaciones.',
-    //siteId: '1467726470533754880',
-    creator: '@ravenbbs',
-    //creatorId: '1467726470533754880',
-    images: {
-      url: '/code-to-image-converter.png',
-      alt: 'Image of code',
-    },
-  },
+twitter: {
+  card: 'summary_large_image',
+  title: 'Code To Image App',
+  description: 'Convierte tu código en imágenes de forma rápida y sencilla con Code To Image App. Ideal para compartir tus proyectos en redes sociales y presentaciones.',
+  siteId: '1467726470533754880',
+  creator: '@ravenbbs',
+  creatorId: '1467726470533754880',
+  images: ['/code-to-image-converter.png'], // Must be an absolute URL
+},
+  
 }
 
 
